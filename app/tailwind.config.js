8aa4b3fb2a64fd9725418b3b369b54c7027427ea
MythicalPhantom <158225349+MythicalPhantom@users.noreply.github.com>
2025-01-05 11:34:48 -0500

    Update tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ["./index.html", "./main.js", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      transform: { 'rotate-y-180': 'rotateY(180deg)', },
+      keyframes: {
+        flip: {
+          '0%': { transform: 'rotateY(0deg)' },
+          '100%': { transform: 'rotateY(180deg)' },
+        },
+      },
+      animation: {
+        flip: 'flip 0.6s forwards',
+      },
     },
   },
   plugins: [require("daisyui")],
